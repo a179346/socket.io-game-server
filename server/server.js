5ed3c618386702e098fd556f03d71379e76effb4
hsiang <hsiang@borden.com.tw>
2020-08-04 18:20:52 +0800

    restructure
    Signed-off-by: hsiang <hsiang@borden.com.tw>

--- server/server.js
+++ server/server.js
@@ -3,79 +3,19 @@ const http = require('http');
 const express = require('express');
 const cors = require('cors');
 require('./system/gracefulShutDown');
-const socketio = require('socket.io');
 const roomStatus = require('./dao/roomStatus');
-const roomBet = require('./dao/roomBet');
+const wsServer = require('./wsServer');
 
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
-const io = socketio(server);
-
-const redisAdapter = require('socket.io-redis');
-const redisConfig = require('./config').redis;
-io.adapter(redisAdapter(redisConfig.pubsub));
-
-const { serverStatus, initServerStatus } = require('./system/serverStatus');
+const { initServerStatus } = require('./system/serverStatus');
 initServerStatus(server);
-
-roomStatus.initValueChangeEvent(io);
-roomBet.initValueChangeEvent(io);
+wsServer(server);
 
 app.get('/roomIds', async(req, res) => {
   const roomIds = await roomStatus.getRoomIds();
   res.send(roomIds);
-});
-
-io.on('connection', socket => {
-  socket.onHandel = function (event, callBack) {
-    socket.on(event, async (...data) => {
-      try {
-        if (serverStatus.status !== 'running' && event !== 'disconnect') throw new Error('server is shutting down');
-        await callBack(...data);
-      } catch (error) {
-        console.error(error);
-        socket.emit('customError', error.message);
-      }
-    });
-  };
-
-  socket.onHandel('joinRoom', async ({ username, roomId }) => {
-    const userId = socket.id;
-    serverStatus.sockets.add(socket);
-    socket.join(roomId);
-
-    const number = await roomStatus.joinRoom(roomId, userId, username);
-    socket.payload = { userId, username, number, roomId };
-
-    io.to(roomId).emit('sysMessage', username + ' join the room.');
-    socket.emit('userId', userId);
-  });
-
-  socket.onHandel('ready', async () => {
-    const { userId, username, roomId, number } = socket.payload;
-    const setPlayerData = {};
-    setPlayerData[`player${number}`] = JSON.stringify({ userId, username, status: roomStatus.PLAYER_STATUS.READY });
-
-    await roomStatus.setRoomStatus(roomId, setPlayerData);
-  });
-
-  socket.onHandel('bet', async (betIndex)=>{
-    const { roomId, number } = socket.payload;
-    await roomBet.bet(roomId, number, betIndex);
-  });
-
-  socket.onHandel('disconnect', async () => {
-    try {
-      serverStatus.sockets.delete(socket);
-    } finally {
-      if (socket.payload) {
-        const { roomId, number } = socket.payload;
-        await roomBet.leaveRoom(roomId, number);
-        await roomStatus.leaveRoom(roomId, number);
-      }
-    }
-  });
 });
 
 server.listen(process.env.PORT, function() {
---
+++ server/wsServer.js
@@ -0,0 +1,64 @@
+module.exports = function (server) {
+  const socketio = require('socket.io');
+  const redisAdapter = require('socket.io-redis');
+  const redisConfig = require('./config').redis;
+  const roomStatus = require('./dao/roomStatus');
+  const roomBet = require('./dao/roomBet');
+  const { serverStatus } = require('./system/serverStatus');
+
+  const io = socketio(server);
+  io.adapter(redisAdapter(redisConfig.pubsub));
+  roomStatus.initValueChangeEvent(io);
+  roomBet.initValueChangeEvent(io);
+
+  io.on('connection', socket => {
+    socket.onHandel = function (event, callBack) {
+      socket.on(event, async (...data) => {
+        try {
+          if (serverStatus.status !== 'running' && event !== 'disconnect') throw new Error('server is shutting down');
+          await callBack(...data);
+        } catch (error) {
+          console.error(error);
+          socket.emit('customError', error.message);
+        }
+      });
+    };
+
+    socket.onHandel('joinRoom', async ({ username, roomId }) => {
+      const userId = socket.id;
+      serverStatus.sockets.add(socket);
+      socket.join(roomId);
+
+      const number = await roomStatus.joinRoom(roomId, userId, username);
+      socket.payload = { userId, username, number, roomId };
+
+      io.to(roomId).emit('sysMessage', username + ' join the room.');
+      socket.emit('userId', userId);
+    });
+
+    socket.onHandel('ready', async () => {
+      const { userId, username, roomId, number } = socket.payload;
+      const setPlayerData = {};
+      setPlayerData[`player${number}`] = JSON.stringify({ userId, username, status: roomStatus.PLAYER_STATUS.READY });
+
+      await roomStatus.setRoomStatus(roomId, setPlayerData);
+    });
+
+    socket.onHandel('bet', async (betIndex)=>{
+      const { roomId, number } = socket.payload;
+      await roomBet.bet(roomId, number, betIndex);
+    });
+
+    socket.onHandel('disconnect', async () => {
+      try {
+        serverStatus.sockets.delete(socket);
+      } finally {
+        if (socket.payload) {
+          const { roomId, number } = socket.payload;
+          await roomBet.leaveRoom(roomId, number);
+          await roomStatus.leaveRoom(roomId, number);
+        }
+      }
+    });
+  });
+};
